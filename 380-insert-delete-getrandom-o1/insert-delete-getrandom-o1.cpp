5b2fc7618a8f959d81fcd52f7e0eb58cd0e1b100
Tarun Pereddi <87849811+TarunPereddi@users.noreply.github.com>
2024-01-16 05:34:08 +0530

    Time: 123 ms (99.67%) | Memory: 95.1 MB (99.03%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -1,14 +1,12 @@
 class RandomizedSet {
 public:
-    unordered_map<int,int> mp; //<num , index at which num is present>
+    unordered_map<int, int> mp;
     vector<int> ans;
-    RandomizedSet() 
-    {
-        ios_base::sync_with_stdio(false);    
+    RandomizedSet() {
+        ios_base::sync_with_stdio(false);
     }
     
-    bool insert(int val) 
-    {
+    bool insert(int val) {
         if(mp.find(val)!=mp.end())
         {
             return false;
@@ -18,26 +16,24 @@ public:
         return true;
     }
     
-    bool remove(int val) 
-    {
+    bool remove(int val) {
         if(mp.find(val)==mp.end())
         {
             return false;
-        }    
-        int index = mp[val]; //index of no that has to be removed
+        }
+        int index = mp[val];
         int last_ele = ans.back();
 
         mp[last_ele] = index;
-        swap(ans[index],ans[ans.size()-1]); //swapping last element and val
+        swap(ans[index], ans[ans.size()-1]);
 
-        ans.pop_back(); //removing val
+        ans.pop_back();
         mp.erase(val);
         return true;
     }
     
-    int getRandom() 
-    {
-        return ans[rand()%ans.size()];    
+    int getRandom() {
+        return ans[rand()%ans.size()];
     }
 };
 
