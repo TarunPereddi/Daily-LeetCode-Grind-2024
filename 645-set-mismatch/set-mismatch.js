a61b333fb5afd4013d3084622da60dc5ece2975f
Tarun Pereddi <87849811+TarunPereddi@users.noreply.github.com>
2024-01-22 09:27:52 +0530

    Time: 50 ms (96.39%) | Memory: 51.4 MB (7.87%) - LeetSync

--- 645-set-mismatch/set-mismatch.js
+++ 645-set-mismatch/set-mismatch.js
@@ -1,11 +1,19 @@
 var findErrorNums = function(nums) {
     let len = nums.length;
-    let sum = (len * (len + 1)) / 2;
-    let sumOfNums = nums.reduce((a, b) => a + b);
-    let uniqueSum = [...new Set(nums)].reduce((a, b) => a + b);
-    
-    let duplicate = sumOfNums - uniqueSum;
-    let missing = sum - uniqueSum;
-    
+    let totalSum = (len * (len + 1)) / 2;
+    let totalSumSq = (len * (len + 1) * (2 * len + 1)) / 6;
+    let sum = 0, sumSq = 0;
+
+    for(let num of nums) {
+        sum += num;
+        sumSq += num * num;
+    }
+
+    let diff = sum - totalSum;
+    let sumDiff = (sumSq - totalSumSq) / diff;
+
+    let duplicate = (diff + sumDiff) / 2;
+    let missing = (sumDiff - diff) / 2;
+
     return [duplicate, missing];
 };
