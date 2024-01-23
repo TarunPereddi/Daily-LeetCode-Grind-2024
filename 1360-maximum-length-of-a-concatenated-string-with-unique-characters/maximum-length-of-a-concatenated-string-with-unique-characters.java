a7167620e2429bea83624d5bdf6462dac4c3b160
Tarun Pereddi <87849811+TarunPereddi@users.noreply.github.com>
2024-01-23 11:32:17 +0530

    Time: 1 ms (100.00%) | Memory: 41.5 MB (92.34%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,37 +1,32 @@
 class Solution {
-    private int result = 0;
+    public int solve(String strs[], int no, int a[], int index, int c) {
+        if(index==c)
+            return 0;
+        int x=no & a[index];
+        if(x==0)        
+            return Math.max(strs[index].length()+solve(strs,no^a[index],a,index+1,c),solve(strs,no,a,index+1,c));
+        else
+            return solve(strs,no,a,index+1,c);
+    }
     public int maxLength(List<String> arr) {
-            if (arr == null || arr.size() == 0)    return 0;
-
-        dfs(arr, "", 0);
-        return result;
-    }
-
-    private void dfs(List<String> arr, String path, int idx) {
-        boolean isUniqueChar = isUniqueChars(path);
-
-        if (isUniqueChar) {
-        result = Math.max(path.length(), result);
+        int a[]=new int[arr.size()];int c=0;String strs[]=new String[a.length];
+        for(String s:arr) {
+            int x=0,fl=0;
+            for(char ch:s.toCharArray()) 
+            {
+                int z=x|(1<<(ch-96));
+                if(x==z)
+                {
+                    fl=1;break;                
+                }
+                x=z;
+            }
+            if(fl==0)
+            {
+                a[c]=x;
+                strs[c++]=s;
+            }
         }
-
-        if (idx == arr.size() || !isUniqueChar) {
-        return;
-        }
-
-        for (int i = idx; i < arr.size(); i++) {
-        dfs(arr, path + arr.get(i), i + 1);
-        }
-
-    }
-
-    private boolean isUniqueChars(String s) {
-        Set<Character> set = new HashSet<>();
-        for (char c : s.toCharArray()) {
-        if (set.contains(c)) {
-            return false;
-        }
-        set.add(c);
-        }
-        return true;
+        return solve(strs,0,a,0,c);
     }
 }
