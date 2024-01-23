9783fd7a6298fad6399cce797523800bc380be9a
Tarun Pereddi <87849811+TarunPereddi@users.noreply.github.com>
2024-01-24 04:21:32 +0530

    Time: 3 ms (84.10%) | Memory: 44.4 MB (23.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,34 +1,18 @@
 class Solution {
-   public boolean isAnagram(String s, String t) {
+    public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
             return false;
         }
 
-        Map<Character, Integer> frequencyMap = new HashMap<>();
+        // Convert strings to char arrays, sort them, and then compare
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
 
-        // Increment frequency for characters in string s
-        for (char c : s.toCharArray()) {
-            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
-        }
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
 
-        // Decrement frequency for characters in string t
-        for (char c : t.toCharArray()) {
-            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) - 1);
-
-            // If the frequency becomes negative, it means the strings are not anagrams
-            if (frequencyMap.get(c) < 0) {
-                return false;
-            }
-        }
-
-        // Check if all frequencies are zero, indicating the strings are anagrams
-        for (int freq : frequencyMap.values()) {
-            if (freq != 0) {
-                return false;
-            }
-        }
-
-        return true;
+        // Compare sorted arrays
+        return Arrays.equals(sArray, tArray);
     }
 
 }
