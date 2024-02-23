20c66821757629452797568af8fbcc0227800b0d
Tarun Pereddi <87849811+TarunPereddi@users.noreply.github.com>
2024-02-23 14:34:40 +0530

    Time: 7 ms (57.71%) | Memory: 44.4 MB (42.97%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         Map<Integer, List<int[]>> adj = new HashMap<>();
